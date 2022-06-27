4240f83785c4507e165c616d3aa239cd9a255b38
Samuel BERTIN <75686202+Samuel-Bertin@users.noreply.github.com>
2022-06-27 21:06:08 +0200

    fix search adv + patth to xampp

--- public_html/php/utils/connexionDB.php
+++ public_html/php/utils/connexionDB.php
@@ -1,6 +1,8 @@
 <?php
 
      function connexion(){
+        $path = 'C:\xampp\htdocs\PTUT\public_html\php';
+        set_include_path(get_include_path() . PATH_SEPARATOR . $path);
         require('config.php');
 
         try {
